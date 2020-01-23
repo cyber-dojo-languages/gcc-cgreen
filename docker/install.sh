97ab0c2f832d2c33ca0b4ef4550ba48b666ec673
Jon Jagger <jon@jaggersoft.com>
2020-01-23 16:33:57 +0000

    Switch to circleci

---
+++ docker/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -Eeu
+
+apt-get install --yes cmake git
+
+git clone https://github.com/cgreen-devs/cgreen.git
+cd cgreen
+cmake .
+make
+make install
+cd ..
+rm -rf cgreen
+
+apt-get remove --yes cmake git
