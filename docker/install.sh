f1b0ca13547768fa59d992762ebe575a8d7050bb
Jon Jagger <jon@jaggersoft.com>
2022-06-26 14:35:02 +0100

    In install.sh add binutils-dev package to get cgreen-runner

--- docker/install.sh
+++ docker/install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -Eeu
 
-apk add cmake git
+apk add binutils-dev
+apk add cmake wget unzip
 wget https://github.com/cgreen-devs/cgreen/archive/refs/tags/1.5.1.zip
 unzip 1.5.1.zip
 cd cgreen-1.5.1
 
 make
-make test
 make install
 cd ..
 
 rm -rf cgreen-1.5.1
-apk del cmake git
-
-
-# Issue cgreen-runner ?
-# https://github.com/cgreen-devs/cgreen/issues/288
+apk del cmake wget unzip
